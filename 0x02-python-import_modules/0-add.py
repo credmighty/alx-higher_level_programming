ba4b48c550e5867896232a2011f44675491ba7f3
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-01-05 03:20:21 +0100

    extbl pycs def file whitespaces

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     """importing the add"""
     from add_0 import add
-    
+
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
