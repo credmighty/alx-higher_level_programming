ee1d3634f64fa4b60cbed781c8002f341082c389
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-01-05 04:22:29 +0100

    worked on small numbers handled big numbers

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    n = len(sys.argv)
+    for i in range(1, n):
+        sum += int(sys.argv[i])
+    print("{}".format(sum))
