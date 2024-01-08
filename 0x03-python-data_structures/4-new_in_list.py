fa50eab2915239ba53f781cd19f22ff28b10134a
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-01-08 23:07:53 +0100

    replace a list without affecting the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    cpy_list = my_list[:]
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        cpy_list[idx] = element
+        return cpy_list
