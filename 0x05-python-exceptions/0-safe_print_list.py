c0e1c49942e8d8811e1f1188462a41c8c2a3fa9a
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-01-23 02:25:13 +0100

    print int upto x --> tab 8 wrong for py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-	c = 0
-	try:
-		for a in range(x):
-			print(my_list[a], end='')
-			c += 1
-	except:
-		pass
-	print()
-	return c 
+    c = 0
+    for a in range(x):
+        try:
+            print("{}".format(my_list[a]), end='')
+            c += 1
+        except IndexError:
+            pass
+    print()
+    return c
