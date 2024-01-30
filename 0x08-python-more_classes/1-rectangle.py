67ed2532f44162b900dc2f212dff589b5c7e8c55
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-01-30 01:08:49 +0100

    decorator arrangement

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,24 +12,24 @@ class Rectangle:
     def width(self):
         return self.__width
 
-    @property
-    def height(self):
-        return self.__height
-
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError('width must be integer')
-        if value < 0:
+        elif value < 0:
             raise ValueError('width must be >= 0')
         else:
             self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError(' height be integer')
-        if value < 0:
+        elif value < 0:
             raise ValueError('height must be >= 0')
         else:
             self.__height = value
