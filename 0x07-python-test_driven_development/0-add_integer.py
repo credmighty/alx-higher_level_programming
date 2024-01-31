023c2486494d4d11455698e3a3361cbba11470f5
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-01-31 07:57:12 +0100

    func documented & output case copied A-message for B

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,8 +3,13 @@
 
 
 def add_integer(a, b=98):
+    """Return the integer addition of a and b
+     Float arguments are typecasted to int before addition is performed
+     Raises:
+         TypeError: {messages}
+    """
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
-        raise TypeError("a must be an integer")
+        raise TypeError("b must be an integer")
     return int(a) + int(b)
