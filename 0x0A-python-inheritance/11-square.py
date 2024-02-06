eb09673eded86a0af46e119c8b7e47da883e4650
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-02-06 08:45:58 +0100

    wrong indented class documented rendered it missing

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,7 +5,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-        """class Square that inherits from Rectangle (9-rectangle.py)"""
+    """class Square that inherits from Rectangle (9-rectangle.py)"""
     def __init__(self, size):
         """initialization method"""
         self.integer_validator('size', size)
@@ -15,3 +15,7 @@ class Square(Rectangle):
     def area(self):
         """area methood to be implemented"""
         return self.__size * self.__size
+
+    def __str__(self):
+        """magic str return, the square desc: [Square] <width>/<height>"""
+        return "[Square] {}/{}".format(self.__size)
