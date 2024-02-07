c6910c52ee93f73af86247b8a128612b167cc310
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-02-07 03:17:09 +0100

    rm import and used json method in the write

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -2,14 +2,11 @@
 """project I/O of 0x0B"""
 import json
 
-to_json_string = __import__('3-to_json_string').to_json_string
-
 
 def save_to_json_file(my_obj, filename):
     """a function that writes an Object to a text file,
     using a JSON representation
     """
-    json_data = to_json_string(my_obj)
 
     with open(filename, 'w', encoding='utf-8') as f:
-        f.write(json_data)
+        f.write(json.dumps(my_obj))
