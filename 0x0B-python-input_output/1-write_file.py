7792c748eb077741c1ac09ea72c8eaf212f096ad
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-02-07 01:53:53 +0100

    pyco flag

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """project I/O of 0x0B"""
 
+
 def write_file(filename="", text=""):
     """function that writes a string to a text file (UTF8)
     and returns the number of characters written"""
