04af4819fefe6231a75091107a8a91969083d518
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-02-07 05:05:27 +0100

    student dk & rld-- advances

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Defines a text file insertion function."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert text after each line containing a given string in a file."""
+    text = ""
+    with open(filename, encoding='utf-8') as f:
+        for lines in f:
+            text += lines
+            if search_string in lines:
+                text += new_string
+
+    with open(filename, "w", encoding='utf-8') as wr:
+        wr.write(text)
---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""project I/O of 0x0B"""
+
+
+class Student:
+    """student class creation"""
+    def __init__(self, first_name, last_name, age):
+        """initialization method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """json method to retrieve dictionary repr"""
+        if attrs is None:
+            return self.__dict__
+        dict_ = {}
+        for values in attrs:
+            if hasattr(self, values):
+                dict_[values] = getattr(self, values)
+        return dict_
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for items in json:
+            self.__dict__[items] = json[items]
