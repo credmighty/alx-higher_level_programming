b4aa4f6d1edcf3b416cac06e418cca6edfeb6af5
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-02-07 01:43:36 +0100

    end used instead of .read to rm last line \n

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,4 +4,5 @@
 def read_file(filename=""):
     """Write a function that reads a text file (UTF8) & prints it to stdout"""
     with open(filename, 'r', encoding='utf-8') as opened_file:
-        print(opened_file.read())
+        for line in opened_file:
+            print(line, end='')
