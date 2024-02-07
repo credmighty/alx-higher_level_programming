702db1a4a1ab96786aad1e9caa4a9854adec7933
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-02-07 02:16:47 +0100

    conversiom to json data

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""project I/O of 0x0B"""
+
+
+def to_json_string(my_obj):
+    """a function that returns the JSON
+    representation of an object (string)"""
+    import json
+
+    return (json.dumps(my_obj))
