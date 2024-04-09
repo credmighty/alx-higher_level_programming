41e659253b211f93d7e9a60e6bcc41be16192953
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-04-10 00:23:43 +0100

    Rectangle & square object

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+    this.width = w;
+    this.height = h;
+    }
+  }
+  print () {
+    let x ='';
+    for (let a = 0; a < this.width; a++) {
+      x += 'X';
+    }
+    for (let b = 0; b < this.height; b++) {
+      console.log(x);
+    }
+  }
+}
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+    this.width = w;
+    this.height = h;
+    }
+  }
+  print () {
+    let x ='';
+    for (let a = 0; a < this.width; a++) {
+      x += 'X';
+    }
+    for (let b = 0; b < this.height; b++) {
+      console.log(x);
+    }
+  }
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
