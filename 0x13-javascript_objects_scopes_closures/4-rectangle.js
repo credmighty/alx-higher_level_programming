e9ec14660bac0e5959fbc7c3b047c422d0ff60d6
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-04-10 05:35:26 +0100

    space after each method -> elint

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -16,11 +16,13 @@ class Rectangle {
       console.log(x);
     }
   }
+
   rotate () {
     const temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
+
   double () {
     this.width *= 2;
     this.height *= 2;
