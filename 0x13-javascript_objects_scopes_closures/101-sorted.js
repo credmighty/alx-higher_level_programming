e9cb2e8dd6289e446c5a01565156ddfbabfb1780
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-04-10 05:13:42 +0100

    tab->elint

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,8 +4,7 @@ const newDict = {};
 for (const key in dict) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [];
-  } else {
+  }
     newDict[dict[key]].push(key);
-	}
 }
 console.log(newDict);
