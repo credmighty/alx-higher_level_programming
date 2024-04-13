614b6feaeb596e857ebbe8fef7cddeb6ee672846
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-04-13 10:17:52 +0100

    list all with MySQL python

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that lists all of MySQL"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    for rows in cur.fetchall():
+        print(rows)
+
+    cur.close()
+    db.close()
