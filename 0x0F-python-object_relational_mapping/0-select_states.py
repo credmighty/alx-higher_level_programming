2ffb39ca862d6bd29987f71e3fbd0284f02b0c2a
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-04-14 08:28:54 +0100

    catching error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,12 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset='utf8')
+    try:
+        db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                             passwd=argv[2], db=argv[3], charset='utf8')
+    except MySQLdb.Error as e:
+        print("Error connecting to database: {}".format(e))
+        sys.exit(1)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     for rows in cur.fetchall():
