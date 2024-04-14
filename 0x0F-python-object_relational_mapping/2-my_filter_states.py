0bee046663e6712bffab374f9fe1ee8779c04916
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-04-14 09:01:59 +0100

    filtering by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that lists all of MySQL"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                             passwd=argv[2], db=argv[3], charset='utf8')
+    except MySQLdb.Error as e:
+        print("Error connecting to database: {}".format(e))
+        sys.exit(1)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' \
+                ORDER BY id ASC".format(argv[4]))
+    for rows in cur.fetchall():
+        """if rows[1][0] == 'N':"""
+        print(rows)
+
+    cur.close()
+    db.close()
