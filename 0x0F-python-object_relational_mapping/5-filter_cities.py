0548197042f2eb1c7475a0c59be724112df71685
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-04-20 06:49:32 +0100

    2nd ddl -pyco

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,7 +8,7 @@ from sys import argv
 
 if __name__ == "__main__":
     db_con = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
+                             passwd=argv[2], db=argv[3], charset="utf8")
     cur = db_con.cursor()
     cur.execute("SELECT cities.name \
     FROM cities \
