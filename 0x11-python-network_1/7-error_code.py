15e22282e3843a7345a2f36245838676c6132415
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-04-27 00:45:49 +0100

    error with requests

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8)."""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    query = requests.get(argv[1])
+    if status_code >= 400:
+        print('Error code: {}'.format(query_.status_code))
+    else:
+        print(query_.text)
