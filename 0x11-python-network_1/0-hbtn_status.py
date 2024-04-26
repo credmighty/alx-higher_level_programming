1a13c8cc36254e68c8b8276ff1b874cbf19bf0de
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-04-26 22:45:15 +0100

    README and task 1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib
+
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    query_ = response.read()
+
+print('Body response:$')
+print('\t- type: {}'.format(type(query_)))
+print('\t- content: {}'.format(query_))
+print('\t- utf8 content: {}'.format(query_.decode('utf-8')))
