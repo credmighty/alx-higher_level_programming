8ec723d3654547c8f495088077928d51237d4ec2
Abdullah Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-04-25 18:01:00 -0700

    100, 101, 102

---
+++ 0x10-python-network_0/100-status_code.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Write a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
+#
+#You are not allowed to use any pipe, redirection, etc.
+#You are not allowed to use ; and &&
+#You have to use curl
+
+curl -so /dev/null -w "%{http_code}" "$1"
