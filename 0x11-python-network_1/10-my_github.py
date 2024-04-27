98527eb81afff7578bfda602b4ec13f5203c608c
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-04-27 01:29:05 +0100

    task 8,9 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id
+"""
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    req = requests.get(url,
+		auth=(argv[1], argv[2]))
+    if req.status_code == 200:
+        print(req.json().get('id'))
+    else:
+        print('None')
---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL and an email, sends a POST
+   request to the passed URL with the email as a parameter, and finally
+   displays the body of the response."""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    if len(argv) == 2: 
+        q = argv[1] 
+	else:
+        q = ""
+    req = resquest.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        req_dict = req.json()
+        id, name = req_dict.get('id'), req_dict.get('name')
+        if len(req_dict == 0 or not id or not name:
+            print("No result")
+        else:
+            print("[{}] {}".format(req_dict.get('id'), req_dict.get('name')))
+    except:
+        print("Not a valid JSON")
