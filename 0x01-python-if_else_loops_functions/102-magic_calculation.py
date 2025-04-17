ec49cabfccbfddd07f81d00422977c63f1108f45
Abdullahi Abdulrofiu <47549097+credmighty@users.noreply.github.com>
2025-04-17 10:47:55 +0100

    Update 102-magic_calculation.py
    pycodestyle correction

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def magic_calculation(a,b,c):
+def magic_calculation(a, b, c):
     """Magic bytecode provided by holberton school"""
     if a < b:
         return (c)
     if c > b:
         return (a + b)
-    return (a*b -c)
+    return (a * b - c)
